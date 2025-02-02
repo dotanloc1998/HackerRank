008bbb29b7c3ca26041f16ecee35c85749b858c0
Loc Do <thuyloc988@gmail.com>
2025-02-02 17:15:28 +0700

    update

--- obj/Debug/net8.0/HackerRank.AssemblyInfo.cs
+++ obj/Debug/net8.0/HackerRank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HackerRank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b9eb8a3ca47f303116cfd87963d558c65291b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b5496d230752ea4d6d0bdb632927ddfe24da1fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("HackerRank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HackerRank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
